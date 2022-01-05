fefbd17dc94e2b321133e2bd530b31a8e31e97fb
heegeepark <phg980513@naver.com>
2022-01-05 17:53:16 +0900

    Feat: Add Assets2 - UIColor 익스텐션 추가 - AppIcon Png 추가

---
+++ GuessMe_iOS/Extension/UIColor+.swift
@@ -0,0 +1,46 @@
+//
+//  UIColor+.swift
+//  GuessMe_iOS
+//
+//  Created by 박희지 on 2022/01/05.
+//
+
+import UIKit
+
+extension UIColor {
+    class var mainColor: UIColor? {
+        UIColor(named: "mainColor")
+    }
+    
+    class var accentColor: UIColor? {
+        UIColor(named: "accentColor")
+    }
+    
+    class var blackColor: UIColor? {
+        UIColor(named: "blackColor")
+    }
+    
+    class var whiteColor: UIColor? {
+        UIColor(named: "whiteColor")
+    }
+    
+    class var grayColor: UIColor? {
+        UIColor(named: "grayColor")
+    }
+    
+    class var ligthGrayColor: UIColor? {
+        UIColor(named: "ligthGrayColor")
+    }
+    
+    class var blueColor: UIColor? {
+        UIColor(named: "blueColor")
+    }
+    
+    class var lightBlueColor: UIColor? {
+        UIColor(named: "lightBlueColor")
+    }
+    
+    class var darkPinkColor: UIColor? {
+        UIColor(named: "darkPinkColor")
+    }
+}
