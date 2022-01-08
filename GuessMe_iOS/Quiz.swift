f10d08cfdae9463f19cdd4ac20eb8c0e480e1f4f
heegeepark <phg980513@naver.com>
2022-01-09 04:55:34 +0900

    Feat: Quiz Model 추가

---
+++ GuessMe_iOS/Quiz.swift
@@ -0,0 +1,14 @@
+//
+//  Quiz.swift
+//  GuessMe_iOS
+//
+//  Created by 박희지 on 2022/01/09.
+//
+
+import Foundation
+
+struct Quiz {
+    var quizId: Int
+    var content: String
+    var answer: Int
+}
