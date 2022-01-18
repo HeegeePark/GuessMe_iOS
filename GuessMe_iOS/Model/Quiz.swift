df67e4b935122ad1f05ea3c2278b1713327fbada
heegeepark <phg980513@naver.com>
2022-01-18 23:47:07 +0900

    Feat: Add Function - 퀴즈 타입을 파라미터로 변환하는 함수

--- GuessMe_iOS/Model/Quiz.swift
+++ GuessMe_iOS/Model/Quiz.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 struct Quiz {
     var quizId: Int
@@ -14,5 +15,14 @@ struct Quiz {
     
     static func getDummy() -> [Quiz]{
         return [Quiz(quizId: 1, content: "CC(Campus Couple)를 한 적이 있다.", answer: 0), Quiz(quizId: 2,content: "파인애플 피자를 좋아하는 것을 숨긴 적 있다.", answer: 0), Quiz(quizId: 3,content: "방에 들어갈 때 '따라다라단~ 따라다라다단~' 한 적이 있다.", answer: 0), Quiz(quizId: 4,content: "집에 혼자 들어가면서 '거기 있는거 다 안다, 나와' 한 적이 있다.", answer: 0), Quiz(quizId: 5,content: "발을 헛디뎌 놓고 아무렇지 않은 척 걸어간 적 있다.", answer: 0)]
+    }
+    
+    public func toParameters() -> Parameters {
+        let dict: Parameters = [
+            "quizId": self.quizId,
+            "content": self.content,
+            "answer": self.answer
+        ]
+        return dict
     }
 }
